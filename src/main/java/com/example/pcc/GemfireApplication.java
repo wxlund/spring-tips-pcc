91718e1f158f9124b46521c878fa7ed686fe2391
wxlund <wxlund@yahoo.com>
2019-01-11 13:50:26 -0800

    added session sample

--- src/main/java/com/example/pcc/GemfireApplication.java
+++ src/main/java/com/example/pcc/GemfireApplication.java
@@ -17,13 +17,20 @@
 
 package com.example.pcc;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.extern.java.Log;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Reference;
@@ -33,68 +40,106 @@ import org.springframework.data.gemfire.config.annotation.EnablePdx;
 import org.springframework.data.gemfire.mapping.annotation.Indexed;
 import org.springframework.data.gemfire.mapping.annotation.Region;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import java.io.Serializable;
-import java.util.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
 
 @Log
 @EnableIndexing
 @EnableEntityDefinedRegions
 @EnablePdx
+@EnableGemFireHttpSession(poolName = "DEFAULT",regionName="Sessions")
 @SpringBootApplication
 public class GemfireApplication {
 
-  private ApplicationRunner titledRunner(String title, ApplicationRunner rr) {
-    return args -> {
-      log.info(title.toUpperCase() + ":");
-      rr.run(args);
-    };
-  }
+	private ApplicationRunner titledRunner(String title, ApplicationRunner rr) {
+		return args -> {
+			log.info(title.toUpperCase() + ":");
+			rr.run(args);
+		};
+	}
 
-  @Bean
-  ApplicationRunner gemfireRepositories(
-          OrderRepository orderRepository,
-          LineItemRepository lineItemRepository) {
+	// skipped geo repository. Need to come back to it.
+	@Bean
+	ApplicationRunner gemfireRepositories(OrderRepository orderRepository, LineItemRepository lineItemRepository) {
 
-    return titledRunner("gemfireRepositories", args -> {
+		return titledRunner("gemfireRepositories", args -> {
 
-      Long orderId = generateId();
+			Long orderId = generateId();
 
-      List<LineItem> itemList = Arrays.asList(
-              new LineItem(orderId, generateId(), "plunger"),
-              new LineItem(orderId, generateId(), "soup"),
-              new LineItem(orderId, generateId(), "coffee mug"));
-      itemList
-              .stream()
-              .map(lineItemRepository::save)
-              .forEach(li -> log.info(li.toString()));
+			List<LineItem> itemList = Arrays.asList(new LineItem(orderId, generateId(), "plunger"),
+					new LineItem(orderId, generateId(), "soup"), new LineItem(orderId, generateId(), "coffee mug"));
+			itemList.stream().map(lineItemRepository::save).forEach(li -> log.info(li.toString()));
 
-      Order order = new Order(orderId, new Date(), itemList);
-      orderRepository.save(order);
+			Order order = new Order(orderId, new Date(), itemList);
+			orderRepository.save(order);
 
-      Collection<Order> found = orderRepository.findByWhen(order.getWhen());
-      found.forEach(o -> log.info("found: " + o.toString()));
-    });
-  }
+			Collection<Order> found = orderRepository.findByWhen(order.getWhen());
+			found.forEach(o -> log.info("found: " + o.toString()));
+		});
+	}
 
-  private Long generateId() {
-    long tmp = new Random().nextLong();
-    return Math.max(tmp, tmp * -1);
-  }
+	// skipped pubsub redis example
 
-  public static void main(String[] args) {
-    SpringApplication.run(GemfireApplication.class, args);
-  }
+	@Bean
+	ApplicationRunner cache(OrderService orderService) {
+		return titledRunner("caching", a -> {
+			Runnable measure = () -> orderService.byId(1L);
+			log.info("first " + measure(measure));
+			log.info("two " + measure(measure));
+			log.info("three " + measure(measure));
+		});
+	}
+
+	private Long generateId() {
+		long tmp = new Random().nextLong();
+		return Math.max(tmp, tmp * -1);
+	}
+
+	private long measure(Runnable r) {
+		long start = System.currentTimeMillis();
+		r.run();
+		long stop = System.currentTimeMillis();
+		return stop - start;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemfireApplication.class, args);
+	}
 
 }
 
+@Service
+class OrderService {
+
+	@Cacheable("Order")
+	public Order byId(Long id) {
+		// @formatter:off
+		try {
+			Thread.sleep(1000 * 10);
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+		// @formatter:on
+		return new Order(id, new Date(), Collections.emptyList());
+	}
+}
+
 interface OrderRepository extends CrudRepository<Order, Long> {
-  Collection<Order> findByWhen(Date d);
+	Collection<Order> findByWhen(Date d);
 }
 
 interface LineItemRepository extends CrudRepository<LineItem, Long> {
 }
-
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -102,16 +147,15 @@ interface LineItemRepository extends CrudRepository<LineItem, Long> {
 @Data
 class Order implements Serializable {
 
-  @Id
-  private Long id;
+	@Id
+	private Long id;
 
-  @Indexed
-  private Date when;
+	@Indexed
+	private Date when;
 
-  @Reference
-  private List<LineItem> lineItems;
+	@Reference
+	private List<LineItem> lineItems;
 }
-
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -119,13 +163,48 @@ class Order implements Serializable {
 @Data
 class LineItem implements Serializable {
 
-  @Indexed
-  private Long orderId;
+	@Indexed
+	private Long orderId;
 
-  @Id
-  private Long id;
+	@Id
+	private Long id;
 
-  private String description;
+	private String description;
+}
+
+@NoArgsConstructor
+class ShoppingCart implements Serializable {
+
+	private final Collection<Order> orders = new ArrayList<>();
+
+	public void addOrder(Order order) {
+		this.orders.add(order);
+	}
+
+	public Collection<Order> getOrders() {
+		return this.orders;
+	}
 }
 
 
+@Log
+@Controller
+@SessionAttributes("cart")
+class CartSessionController {
+
+	private final AtomicLong ids = new AtomicLong();
+
+	@ModelAttribute("cart")
+	ShoppingCart cart() {
+		log.info("creating new cart");
+		return new ShoppingCart();
+	}
+
+	@GetMapping("/orders")
+	String orders(@ModelAttribute("cart") ShoppingCart cart,
+	              Model model) {
+		cart.addOrder(new Order(ids.incrementAndGet(), new Date(), Collections.emptyList()));
+		model.addAttribute("orders", cart.getOrders());
+		return "orders";
+	}
+}
